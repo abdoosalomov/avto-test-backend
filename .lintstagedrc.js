c3f9efd7f1d78302a6520309b59bc9a24b327c54
A'zamjon Abdusalomov <a.abdusalomov@agrobank.uz>
2025-08-12 20:18:35 +0500

    refactor: enhance linting and configuration & remove unused app.service

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,4 +1,4 @@
 module.exports = {
-    '*.{js,jsx,ts,tsx}': ['prettier --write', 'eslint --fix'],
+    '*.{js,jsx,ts,tsx}': ['prettier --write', 'eslint --fix', 'npm run build'],
     '*.{json,md,yml,yaml}': ['prettier --write'],
 };
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,22 +1,23 @@
 // @ts-check
 import eslint from '@eslint/js';
-import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
-import globals from 'globals';
 import tseslint from 'typescript-eslint';
-
 export default tseslint.config(
     {
-        ignores: ['eslint.config.mjs', '.lintstagedrc.js', 'commitlint.config.js', '*.config.js', '*.config.mjs'],
+        ignores: [
+            'eslint.config.mjs',
+            '.lintstagedrc.js',
+            'commitlint.config.js',
+            '*.config.js',
+            '*.config.mjs',
+            '**/*.config.js',
+            '**/*.config.mjs',
+        ],
     },
     eslint.configs.recommended,
-    ...tseslint.configs.recommendedTypeChecked,
-    eslintPluginPrettierRecommended,
+    tseslint.configs.recommended,
+    ...tseslint.configs.all,
     {
         languageOptions: {
-            globals: {
-                ...globals.node,
-                ...globals.jest,
-            },
             ecmaVersion: 5,
             sourceType: 'module',
             parserOptions: {
@@ -27,9 +28,47 @@ export default tseslint.config(
     },
     {
         rules: {
-            '@typescript-eslint/no-explicit-any': 'off',
-            '@typescript-eslint/no-floating-promises': 'warn',
-            '@typescript-eslint/no-unsafe-argument': 'warn',
+            '@typescript-eslint/no-explicit-any': 'error',
+            '@typescript-eslint/no-floating-promises': 'error',
+            '@typescript-eslint/no-unsafe-argument': 'error',
+            '@/array-callback-return': 'error',
+            '@/getter-return': 'error',
+            '@/no-await-in-loop': 'error',
+            '@/no-const-assign': 'error',
+            '@/no-duplicate-imports': 'off',
+            '@/no-setter-return': 'error',
+            '@/no-unreachable': 'error',
+            '@/no-unused-vars': 'error',
+            '@typescript-eslint/naming-convention': [
+                'error',
+                {
+                    selector: 'function',
+                    format: ['camelCase', 'PascalCase'], // Allow PascalCase for functions
+                },
+            ],
+            '@/block-scoped-var': 'error',
+            '@/default-case': 'warn',
+            '@/eqeqeq': 'error',
+            '@/no-console': 'warn',
+            '@/no-undefined': 'error',
+            '@/no-unused-expressions': 'error',
+            '@typescript-eslint/explicit-function-return-type': 'error',
+            '@typescript-eslint/parameter-properties': 'off',
+            '@typescript-eslint/prefer-readonly-parameter-types': 'off',
+            '@typescript-eslint/explicit-member-accessibility': 'off',
+            '@typescript-eslint/class-methods-use-this': 'off',
+            '@typescript-eslint/no-extraneous-class': 'off',
+            '@typescript-eslint/init-declarations': 'off',
+            '@typescript-eslint/no-magic-numbers': [
+                'error',
+                {
+                    ignore: [0, 1, -1], // Example valid numbers to ignore
+                    ignoreArrayIndexes: true, // Correctly specify this option
+                    ignoreClassFieldInitialValues: true, // Correctly specify this option
+                },
+            ],
+            '@typescript-eslint/max-params': 'off',
+            '@typescript-eslint/no-deprecated': 'off',
         },
     },
 );
