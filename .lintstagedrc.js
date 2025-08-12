999fe94b386ffb3986b36aee7aaa88e0184e3bdd
A'zamjon Abdusalomov <a.abdusalomov@agrobank.uz>
2025-08-12 20:33:02 +0500

    refactor: update dependencies and improve configuration for app

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,4 +1,4 @@
 module.exports = {
-    '*.{js,jsx,ts,tsx}': ['prettier --write', 'eslint --fix', 'npm run build'],
+    '*.{js,jsx,ts,tsx}': ['prettier --write', 'eslint --fix', 'pnpm run build'],
     '*.{json,md,yml,yaml}': ['prettier --write'],
 };
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,31 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import type { OpenAPIObject } from '@nestjs/swagger';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap(): Promise<void> {
-    const PORT = 3000;
+    const APP_DEFAULT_PORT = 3000;
     const app = await NestFactory.create(AppModule);
-    await app.listen(PORT);
+
+    const config = app.get(ConfigService);
+    const logger = new Logger('NestApplication');
+
+    const PORT = config.get<number>('APP_PORT', APP_DEFAULT_PORT);
+
+    const swaggerConfig = new DocumentBuilder()
+        .setTitle('Auto test backend')
+        .setDescription('Auto test backend API documentation')
+        .build();
+    const documentFactory = (): OpenAPIObject => SwaggerModule.createDocument(app, swaggerConfig);
+    SwaggerModule.setup('docs', app, documentFactory);
+
+    app.setGlobalPrefix('api');
+
+    await app.listen(PORT).then((): void => {
+        logger.log(`Server is running on port: ${PORT}`);
+    });
 }
 
 void bootstrap();
